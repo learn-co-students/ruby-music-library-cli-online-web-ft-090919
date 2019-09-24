98c2eeac2774e711caafa9433aa1536b2f1389f7
Derrie N <ewenaiwe@gmail.com>
2019-09-24 19:23:01 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -9,10 +9,25 @@ class MusicLibraryController
     MusicImporter.new(path).import
   end
   
+  def list_songs_by_genre
+    puts "Please enter the name of an genre:"
+    input = gets.strip
+    if artist = Artist.find_by_name(input)
+      artist.songs.sort{|a, b| a.name <=> b.name}.each.with_index(1) do |song, index|
+        puts "#{index}. #{song.name} - #{song.genre.name}"
+      end
+    end
+  end
+    
   
   def list_songs_by_artist
     puts "Please enter the name of an artist:"
     input = gets.strip
+    if artist = Artist.find_by_name(input)
+      artist.songs.sort{|a, b| a.name <=> b.name}.each.with_index(1) do |song, index|
+        puts "#{index}. #{song.name} - #{song.genre.name}"
+      end
+    end
   end
 
   def list_genres
