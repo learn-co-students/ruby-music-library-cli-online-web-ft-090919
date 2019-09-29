0a969196238b06bbad1a844f3ada8d680bb9912e
Rsja12 <asocarras10@gmail.com>
2019-09-29 12:33:30 -0400

    Done.

--- lib/musicLibraryController.rb
+++ lib/musicLibraryController.rb
@@ -37,8 +37,8 @@ class MusicLibraryController
     end
   
     def list_songs
-      Song.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
-        puts "#{i}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
+      Song.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |song, i|
+        puts "#{i}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
       end
     end
   
