2ee573e7c1aaa8d9a9c7a80adf8559f9816b3306
Nightrose2016 <lrbarrer@gmail.com>
2019-09-29 13:12:11 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -5,6 +5,8 @@ module Concerns
 
 end
 
-require_relative '../lib/concerns/findable'
+# require_relative '../lib/concerns/findable'
+# require_relative '../lib/concerns/memorable'
+# require_relative '../lib/concerns/paramable'
 
 require_all 'lib'
--- lib/Concerns/findable.rb
+++ lib/Concerns/findable.rb
@@ -1,10 +1,10 @@
-module Concerns::Findable
+module Findable
 
     def find_by_name(name)
-      self.all.detect{|i| i.name == name}
+      all.all.detect{|i| i.name == name}
     end
   
     def find_or_create_by_name(name)
-      self.find_by_name(name) || self.create(name)
+      all.find_by_name(name) || self.create(name)
     end
 end
